d8cf78a3f51ef0dc2b0326f1e0c73317c3af0caa
Victor Higoy <higoyvictor143@gmail.com>
2023-07-13 17:39:12 +0800

    dfdf

--- app/page.tsx
+++ app/page.tsx
@@ -4,34 +4,14 @@ import About from "@/components/About";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
 import Footer from "@/components/Footer";
-import type { Metadata } from "next";
+import { metaBuilder } from "@/helpers/metabuilders";
 
-export const metadata = {
-  title: "Victor Higoy Jr",
-  description: "Frontend Developer based in the Philippines",
-  openGraph: {
+export async function generateMetadata() {
+  return metaBuilder({
     title: "Victor Higoy Jr",
     description: "Frontend Developer based in the Philippines",
-    url: "https://nextjs.org",
-    siteName: "Next.js",
-    images: [
-      {
-        url: "/img/VictorPortfolioMetaBuilder.png",
-        width: 800,
-        height: 600,
-      },
-      {
-        url: "/img/VictorPortfolioMetaBuilder.png",
-        width: 1800,
-        height: 1600,
-        alt: "My custom alt",
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-};
-
+  });
+}
 function Home() {
   return (
     <div className="w-full overflow-hidden">
--- helpers/metabuilders.ts
+++ helpers/metabuilders.ts
@@ -27,7 +27,7 @@ export function metaBuilder({
       siteName: "Fremont Sign Company",
       images: [
         {
-          url: imgUrl ? imgUrl : "/img/VictorPortfolioMetaBuilder.png",
+          url: imgUrl ? imgUrl : "/public/img/VictorPortfolioMetaBuilder.png",
           width: imgWidth ? imgWidth : "1920",
           height: imgHeight ? imgHeight : "1080",
         },

a9d5cefa178146a5be18062a167fd9de18ad4ce4
Victor Higoy <higoyvictor143@gmail.com>
2023-07-13 16:42:44 +0800

    ss

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,25 @@ import About from "@/components/About";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
 import Footer from "@/components/Footer";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Victor Higoy",
+  description: "Frontend Developer based in the Philippines",
+  openGraph: {
+    url: "https://nextjs.org",
+    siteName: "Next.js",
+    images: [
+      {
+        url: "/img/VictorPortfolioMetaBuilder.png",
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
 
 function Home() {
   return (
